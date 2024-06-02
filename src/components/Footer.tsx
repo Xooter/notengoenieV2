ee51fdd18e2428855a3a30ab4d595a1d6b9e1276
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-06-01 21:20:53 -0300

    Footer credit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
 export const Footer = () => {
   return (
-    <div className="font-light text-sm">
-      2008-2024{" "}
-      <a className="strong" href="https://lucasworcel.com/">
-        lucasworcel
+    <div className="flex justify-between">
+      <div className="font-light text-sm">
+        2008-2024{" "}
+        <a className="strong" href="https://lucasworcel.com/">
+          lucasworcel
+        </a>
+      </div>
+
+      <a className="strong font-light text-sm" href="https://github.com/Xooter">
+        Joaquin Righetti
       </a>
     </div>
   );
